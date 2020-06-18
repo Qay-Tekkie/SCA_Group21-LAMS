f8e663f1c237f28c816a08e8ced47147fed6b199
Qay-Tekkie <gkay260@gmail.com>
2020-06-18 03:15:31 +0100

    Created initial routes

---
+++ app.py
@@ -0,0 +1,137 @@
+from flask import Flask, jsonify,request
+from flask_restful import Resource, reqparse
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
+from passlib.hash import pbkdf2_sha256 as sha256
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'some-secret-string'
+
+db = SQLAlchemy(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+jwt = JWTManager(app)
+
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('username', help = 'This field cannot be blank', required = True)
+parser.add_argument('email', help = 'This field cannot be blank', required = True)
+parser.add_argument('password', help = 'This field cannot be blank', required = True)
+
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(120), unique = True, nullable = False)
+    email = db.Column(db.String(200), unique = True, nullable = False)
+    password = db.Column(db.String(120), nullable = False)
+    
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username = username).first()
+    
+    @classmethod
+    def return_all(cls):
+        def to_json(x):
+            return {
+                'username': x.username,
+                'email': x.email,
+                'password': x.password
+            }
+        return {'users': list(map(lambda x: to_json(x), UserModel.query.all()))}
+
+    @classmethod
+    def delete_all(cls):
+        try:
+            num_rows_deleted = db.session.query(cls).delete()
+            db.session.commit()
+            return {'message': '{} row(s) deleted'.format(num_rows_deleted)}
+        except:
+            return {'message': 'Something went wrong'}
+
+    @staticmethod
+    def generate_hash(password):
+        return sha256.hash(password)
+    
+    @staticmethod
+    def verify_hash(password, hash):
+        return sha256.verify(password, hash)
+
+
+class UserRegistration(Resource):
+    def post(self):
+        data = parser.parse_args()
+        
+        if UserModel.find_by_username(data['username']):
+            return {'message': 'User {} already exists'.format(data['username'])}
+        
+        new_user = UserModel(
+            username = data['username'],
+            email = data['email'],
+            password = UserModel.generate_hash(data['password'])
+        )
+        
+        try:
+            new_user.save_to_db()
+            # access_token = create_access_token(identity = data['username'])
+            # refresh_token = create_refresh_token(identity = data['username'])
+            return {
+                'message': 'User {} was created'.format(data['username']),
+                # 'access_token': access_token,
+                # 'refresh_token': refresh_token
+                }
+        except:
+            return {'message': 'Something went wrong'}, 500
+
+
+class UserLogin(Resource):
+    def post(self):
+        data = parser.parse_args()
+        current_user = UserModel.find_by_username(data['username'])
+
+        if not current_user:
+            return {'message': 'User {} doesn\'t exist'.format(data['username'])}
+        
+        if UserModel.verify_hash(data['password'], current_user.password):
+            # access_token = create_access_token(identity = data['username'])
+            # refresh_token = create_refresh_token(identity = data['username'])
+            return {
+                'message': 'Logged in as {}'.format(current_user.username),
+                # 'access_token': access_token,
+                # 'refresh_token': refresh_token
+                }
+        else:
+            return {'message': 'Wrong credentials'}
+
+class AllUsers(Resource):
+    def get(self):
+        return UserModel.return_all()
+    
+    def delete(self):
+        return UserModel.delete_all()
+
+
+api.add_resource(UserRegistration, '/registration')
+api.add_resource(UserLogin, '/login')
+api.add_resource(AllUsers, '/users')
+
+if __name__ == '__main__':
+    app.run(debug=True)
